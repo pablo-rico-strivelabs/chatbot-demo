d1de8e54b0b8fcbd5a2f26163acece2c4d8e1bcb
Pablo Rico <pablo.rico@strivelabs.io>
2025-11-26 12:02:56 +0100

    improve react tools provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from "next/app";
 import "@/src/app/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient())
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Component {...pageProps} />
-		</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
 	);
 }
